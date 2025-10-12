1fce6b096a575dc2bebeb02288e6d45b85b1e943
DongWoonKim <01030985843z@naver.com>
2025-10-12 14:58:21 +0900

    add create

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import type { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    console.log('title', title);
+    console.log('description', description);
+    return this.boardsService.createBoard(title, description);
+  }
 }
